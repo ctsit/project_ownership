4b55b1a85f6ca07ba5f8a2df80dbfa33a6fb1533
Philip Chase <pbc@ufl.edu>
2018-03-14 16:22:08 -0400

    Add paid_creators table, set owner to creator as step 2 of backfill, and fix logic in PI section

--- doc/backfill_project_ownership.sql
+++ doc/backfill_project_ownership.sql
@@ -24,15 +24,58 @@ select project_id, project_pi_username from redcap_projects where project_pi_use
 -- List the owners to be set
 select project_id, project_pi_username, project_pi_email, project_pi_firstname, project_pi_lastname
   from redcap_projects as rcp left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
-  where project_pi_email is not null and project_pi_email != "" and (rcpo.email is null or rcpo.email != "");
+  where project_pi_email is not null and project_pi_email != "" and (rcpo.email is null or rcpo.email = "");
 -- set the owner to the PI
 insert into rcpo_test (pid, username, email, firstname, lastname)
   select project_id, project_pi_username, project_pi_email, project_pi_firstname, project_pi_lastname
   from redcap_projects as rcp left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
-  where project_pi_email is not null and project_pi_email != "" and (rcpo.email is null or rcpo.email != "");
+  where project_pi_email is not null and project_pi_email != "" and (rcpo.email is null or rcpo.email = "");
+
+
+-- Build a table of the usernames of peole who were professional, fulltime project creators.alter
+-- These people should not own projects except as a last resort.  They create lots of projects and
+-- they use a lot of projects, but they are owner of only a small percentage of their work.
+drop table if exists paid_creators;
+create table paid_creators (
+  username  VARCHAR(128),
+  primary key (username)
+)
+collate utf8_unicode_ci;
+
+insert into paid_creators (username) values ("tls");
+insert into paid_creators (username) values ("j.johnston");
+insert into paid_creators (username) values ("cabernat");
+insert into paid_creators (username) values ("swehmeyer");
 
 
 -- Creator: If owner is null and creator is not suspended and creator is not in paid_creators, then set owner to creator
+-- Enumerate creators
+select rcp.project_id, rcui.username
+from redcap_projects as rcp inner join redcap_user_information as rcui on (rcp.created_by = rcui.ui_id)
+where created_by is not null;
+-- show suspended column
+select username,user_suspended_time from redcap_user_information;
+-- List owners to be set
+select rcp.project_id, rcui.username
+from redcap_projects as rcp
+  inner join redcap_user_information as rcui on (rcp.created_by = rcui.ui_id)
+  left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
+  left join paid_creators as pc on (pc.username = rcui.username)
+where (rcpo.email is null or rcpo.email = "")
+  and rcui.user_suspended_time is not null
+  and pc.username is null;
+-- Set owner to creator
+insert into rcpo_test (pid, username, email, firstname, lastname)
+    select rcp.project_id, rcui.username, rcui.user_email, rcui.user_firstname, rcui.user_lastname
+    from redcap_projects as rcp
+      inner join redcap_user_information as rcui on (rcp.created_by = rcui.ui_id)
+      left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
+      left join paid_creators as pc on (pc.username = rcui.username)
+    where (rcpo.email is null or rcpo.email = "")
+      and rcui.user_suspended_time is not null
+      and pc.username is null;
+
+
 -- Last_user is designer and authz manager: If owner is null and last_user is not suspended and last_user is not in paid_creators, and last_user has design and user_rights permissions on project, then set owner to last_user
 -- Last_user is designer or authz manager: If owner is null and last_user is not suspended and last_user is not in paid_creators, and last_user has design or user_rights permissions on project, then set owner to last_user
 -- Any authz’d user who is not suspended: If owner is null and list of non-suspended, authz’d users is not null, then set owner to a random entry from that list.
@@ -40,3 +83,5 @@ insert into rcpo_test (pid, username, email, firstname, lastname)
 -- Last_user is designer or authz manager but suspended: If owner is null and last_user is not in paid_creators, and last_user has design or user_rights permissions on project, then set owner to last_user
 -- Creator, but suspended: If owner is null and creator is not in paid_creators, then set owner to creator
 -- Paid Creator: If owner is null and creator is not suspended and creator is in paid_creators, then set owner to creator
+
+select * FROM RCPO_TEST;
