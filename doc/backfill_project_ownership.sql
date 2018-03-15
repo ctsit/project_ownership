15eb6eadb6ee8ac882047627581a928f84cd5656
Philip Chase <pbc@ufl.edu>
2018-03-15 16:14:10 -0400

    Add 'Last_user is designer or authz manager' to project owners backfill

--- doc/backfill_project_ownership.sql
+++ doc/backfill_project_ownership.sql
@@ -80,8 +80,44 @@ insert into rcpo_test (pid, username, email, firstname, lastname)
       and pc.username is null;
 
 
--- Last_user is designer and authz manager: If owner is null and last_user is not suspended and last_user is not in paid_creators, and last_user has design and user_rights permissions on project, then set owner to last_user
--- Last_user is designer or authz manager: If owner is null and last_user is not suspended and last_user is not in paid_creators, and last_user has design or user_rights permissions on project, then set owner to last_user
+-- Fix collation in redcap_project_stats
+alter table redcap_project_stats
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+
+-- Last_user is designer or authz manager:
+--   If owner is null and last_user is not suspended and last_user is not in paid_creators, and last_user has design or user_rights permissions on project, then set owner to last_user
+-- enumerate relevant permissions of the last unsuspended user by project
+select rcp.project_id, rcps.last_user, rcur.design, rcur.user_rights, rcuro.design, rcuro.user_rights, rcui.user_suspended_time
+from redcap_projects as rcp
+inner join redcap_project_stats as rcps on (rcp.project_id = rcps.project_id)
+inner join redcap_user_information as rcui on (rcps.last_user = rcui.username)
+left join redcap_user_rights as rcur on (rcp.project_id = rcur.project_id and rcps.last_user = rcur.username)
+left join redcap_user_roles as rcuro on (rcp.project_id = rcuro.project_id and rcur.role_id = rcuro.role_id)
+left join paid_creators as pc on (pc.username = rcui.username)
+left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
+where (rcpo.email is null or rcpo.email = "")
+    and rcui.user_suspended_time is null
+    and pc.username is null
+  and rcps.last_user is not null
+  and (rcur.design = 1 or rcur.user_rights = 1 or rcuro.design = 1 or rcuro.user_rights = 1);
+
+-- set owner to last unsuspended user with some perms
+insert into rcpo_test (pid, username, email, firstname, lastname)
+    select rcp.project_id, rcui.username, rcui.user_email, rcui.user_firstname, rcui.user_lastname
+    from redcap_projects as rcp
+    inner join redcap_project_stats as rcps on (rcp.project_id = rcps.project_id)
+    inner join redcap_user_information as rcui on (rcps.last_user = rcui.username)
+    left join redcap_user_rights as rcur on (rcp.project_id = rcur.project_id and rcps.last_user = rcur.username)
+    left join redcap_user_roles as rcuro on (rcp.project_id = rcuro.project_id and rcur.role_id = rcuro.role_id)
+    left join paid_creators as pc on (pc.username = rcui.username)
+    left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
+    where (rcpo.email is null or rcpo.email = "")
+        and rcui.user_suspended_time is null
+        and pc.username is null
+        and rcps.last_user is not null
+        and (rcur.design = 1 or rcur.user_rights = 1 or rcuro.design = 1 or rcuro.user_rights = 1);
+
 -- Any authz’d user who is not suspended: If owner is null and list of non-suspended, authz’d users is not null, then set owner to a random entry from that list.
 -- Last_user is designer and authz manager but suspended: If owner is null and last_user is not in paid_creators, and last_user has design and user_rights permissions on project, then set owner to last_user
 -- Last_user is designer or authz manager but suspended: If owner is null and last_user is not in paid_creators, and last_user has design or user_rights permissions on project, then set owner to last_user
@@ -89,3 +125,5 @@ insert into rcpo_test (pid, username, email, firstname, lastname)
 -- Paid Creator: If owner is null and creator is not suspended and creator is in paid_creators, then set owner to creator
 
 select * FROM RCPO_TEST;
+
+select * from redcap_projects;
